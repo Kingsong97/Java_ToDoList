5c88bc3ba2ee865ea4cb08989233527d3a046386
kingsong <kingsongsm97@gmail.com>
2024-09-23 14:14:15 +0900

    First Commit

---
+++ src/main/java/com/example/demo/todo/model/Todo.java
@@ -0,0 +1,17 @@
+package com.example.demo.todo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed;
+}
